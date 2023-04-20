7edb629380130327a9944a1cfb5a09532a7f0ac7
ritamtbsilva <ritamtbsilva@gmail.com>
2023-04-20 16:30:49 +0100

    change version

--- R/betASapp.R
+++ R/betASapp.R
@@ -58,7 +58,7 @@ betASapp_ui <- function(){
     titlePanel("betAS: intuitive visualisation of differential alternative splicing"),
 
     #h5(strong(div("(development version)", style = "color: #E0E0E0"))),
-    h5(strong(div("v0.5.0", style = "color: #949494"))),
+    h5(strong(div("v1.0.0", style = "color: #949494"))),
 
     tabsetPanel(
 
